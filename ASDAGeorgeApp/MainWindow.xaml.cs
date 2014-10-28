f79bb7b38f5ff4cac60f381a2be2437bc52820f6
Mark Stevenson <ghostt.tw@gmail.com>
2014-10-28 00:09:38 +0000

    The beginning
    Started work on the basic layout of the Landing Page, removed
    unnecessary code from template

--- ASDAGeorgeApp/MainWindow.xaml.cs
+++ ASDAGeorgeApp/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +25,16 @@ namespace ASDAGeorgeApp
         {
             InitializeComponent();
         }
+
+        private void ExitApplication_Click(object sender, RoutedEventArgs e)
+        {
+            this.Window_Closing(sender, new CancelEventArgs(true));
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
     }
 }
